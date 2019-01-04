ddaf712a2792dea2d293b09fa841a864a59158ec
droconnel22 <droconnel22@gmail.com>
2019-01-04 16:25:20 -0500

    Initial Import from Start Spring Generation. Included Web, JPA, MySql, and Secuity

---
+++ src/test/java/com/dennis/oconnell/samplejavaapi/SampleJavaApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dennis.oconnell.samplejavaapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleJavaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
